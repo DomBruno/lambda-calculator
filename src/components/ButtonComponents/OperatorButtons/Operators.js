429f5aab045616a1e408d17ea51a60c920dbb14b
Dominick Bruno <dbruno@unseen.is>
2019-07-26 20:45:49 -0400

    Steps 1 & 2 completed

--- src/components/ButtonComponents/OperatorButtons/Operators.js
+++ src/components/ButtonComponents/OperatorButtons/Operators.js
@@ -1,10 +1,11 @@
-import React from "react";
-
+import React, {useState} from "react";
+import OperatorButton from "./OperatorButton";
 //import any components needed
-
+import {operators} from "../../../data";
 //Import your array data to from the provided data file
 
 const Operators = () => {
+  const [operatorState, setOperatorState] = useState(operators);
   // STEP 2 - add the imported data to state
   return (
     <div>
