64607439d8c247a65dc7dbc2abc2cc79d7911215
Dominick Bruno <dbruno@unseen.is>
2019-07-26 21:21:12 -0400

    added .map methods

--- src/components/ButtonComponents/NumberButtons/Numbers.js
+++ src/components/ButtonComponents/NumberButtons/Numbers.js
@@ -5,13 +5,15 @@ import NumberButton from "./NumberButton";
 //Import your array data to from the provided data file
 import {numbers} from "../../../data";
 
-const Numbers = () => {
+const Numbers = (props) => {
   const [numberState, setNumberState] = useState(numbers);
   return (
     <div>
-      {/* STEP 3 - Use .map() to iterate over your array data and return a button
-       component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+      {props.numbers.map(number => {
+        return <NumberButton number={number} />;
+      })}
     </div>
   );
 };
+
+export default Numbers;
